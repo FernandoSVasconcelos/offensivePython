9a7f758edc1b6259c23bc2a5744e103b32ac8027
Fernando <fernando_souza_vasconcelos@hotmail.com>
2022-06-21 10:16:41 -0300

    Alterações port scanner

--- network_hacking/port_scanner.py
+++ network_hacking/port_scanner.py
@@ -2,11 +2,15 @@ import socket
 
 def scan():
 	s = socket.socket()
-	for i in range(80):
+	abertas = []
+	for i in range(9000):
 		if(s.connect_ex(("192.168.2.5", i)) == 0):
 			print(f"Porta {i} aberta")
+			abertas.append(i)
 		else:
 			print(f"Porta {i} fechada")
+	print(f"-----------[Portas Abertas]------------")
+	print(abertas)
 
 if __name__ == '__main__':
 	scan()
